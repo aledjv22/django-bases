c7b1213c9270504f7bcf162ab819031071c5ec50
Alejandro Díaz <victor.diaz.jauregui@mi.unc.edu.ar>
2024-04-06 20:11:33 -0300

    Django admin

--- myapp/models.py
+++ myapp/models.py
@@ -4,11 +4,14 @@ from django.db import models
 class Project(models.Model):
   name = models.CharField(max_length=200)
 
+  def __str__(self): # Este método es para que en el admin se muestre el nombre del proyecto
+    return self.name
+
 
 class Task(models.Model):
   title = models.CharField(max_length=200)
   description = models.TextField()
   project = models.ForeignKey(Project, on_delete=models.CASCADE)
 
-  def __str__(self):
-    return self.title
+  def __str__(self): # Este método es para que en el admin se muestre el nombre de la tarea
+    return self.title + ' - ' + self.project.name # Aquí se muestra el nombre de la tarea y el nombre del proyecto al que pertenece
