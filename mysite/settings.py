faa87f771d9d987ab6aa1824f0e0a54f88455757
Alejandro Díaz <victor.diaz.jauregui@mi.unc.edu.ar>
2024-04-06 14:08:13 -0300

    urls y migraciones

--- mysite/settings.py
+++ mysite/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'myapp', # Add this line to the list of installed apps
 ]
 
 MIDDLEWARE = [
