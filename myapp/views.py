bb455b9accc2bf4fb120408329db74678503a667
Alejandro Díaz <victor.diaz.jauregui@mi.unc.edu.ar>
2024-04-07 00:49:28 -0300

    Modularizado

--- myapp/views.py
+++ myapp/views.py
@@ -23,7 +23,7 @@ def hello(request, username):
 def projects(request):
   # projects = list(Project.objects.values())
   projects = Project.objects.all() # Devuelve todos los objetos de la tabla
-  return render(request, 'projects.html', {
+  return render(request, 'projects/projects.html', {
     'projects': projects,
   })
 
@@ -31,13 +31,13 @@ def tasks(request):
   # task = Task.objects.get(id=id) # Asi sería sin una buena vista del 404
   # task = get_object_or_404(Task, id=id)
   tasks = Task.objects.all()
-  return render(request, 'tasks.html', {
+  return render(request, 'tasks/tasks.html', {
     'tasks': tasks,
   })
 
 def create_task(request):
   if request.method == 'GET':
-    return render(request, 'create_task.html', {
+    return render(request, 'tasks/create_task.html', {
       'form': CreateNewTask(),
     })
   else:
@@ -46,3 +46,4 @@ def create_task(request):
     project_id=1)
 
     return redirect('/tasks') # Redirige a la vista tasks
+
