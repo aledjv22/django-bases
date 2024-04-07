84f37a944720383552ca806d4f4c10d7e4cf3674
Alejandro Díaz <victor.diaz.jauregui@mi.unc.edu.ar>
2024-04-06 22:04:01 -0300

    Jinja conditionals

--- myapp/models.py
+++ myapp/models.py
@@ -12,6 +12,7 @@ class Task(models.Model):
   title = models.CharField(max_length=200)
   description = models.TextField()
   project = models.ForeignKey(Project, on_delete=models.CASCADE)
+  done = models.BooleanField(default=False)
 
   def __str__(self): # Este método es para que en el admin se muestre el nombre de la tarea
     return self.title + ' - ' + self.project.name # Aquí se muestra el nombre de la tarea y el nombre del proyecto al que pertenece
