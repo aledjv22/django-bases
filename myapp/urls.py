e131258288b147f886e2622bac4e6e1e2ffa9aa6
Alejandro Díaz <victor.diaz.jauregui@mi.unc.edu.ar>
2024-04-06 23:46:03 -0300

    Formularios

--- myapp/urls.py
+++ myapp/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('hello/<str:username>', views.hello),
     path('projects/', views.projects),
     path('tasks/', views.tasks),
+    path('create_task/', views.create_task),
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import render, redirect
 from .models import Project, Task
+from .forms import CreateNewTask
 
 # Create your views here.
 
@@ -33,3 +34,15 @@ def tasks(request):
   return render(request, 'tasks.html', {
     'tasks': tasks,
   })
+
+def create_task(request):
+  if request.method == 'GET':
+    return render(request, 'create_task.html', {
+      'form': CreateNewTask(),
+    })
+  else:
+    Task.objects.create(title=request.POST['title'], 
+    description=request.POST['description'], 
+    project_id=1)
+
+    return redirect('/tasks') # Redirige a la vista tasks
