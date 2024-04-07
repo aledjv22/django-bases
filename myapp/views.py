02c6f631ca315845cc82d953af1ab1c0732e0439
Alejandro Díaz <victor.diaz.jauregui@mi.unc.edu.ar>
2024-04-06 21:13:57 -0300

    Templates

--- myapp/views.py
+++ myapp/views.py
@@ -5,17 +5,26 @@ from .models import Project, Task
 # Create your views here.
 
 def index(request):
-  return render(request, 'index.html')
+  title = 'Django Course!!!'
+  return render(request, 'index.html', {
+    'title': title,
+  })
 
 def about(request):
-  return render(request, 'about.html')
+  username = 'Alejandro'
+  return render(request, 'about.html', {
+    'username': username,
+  })
 
 def hello(request, username):
   return HttpResponse("<h1>Hello %s</h1>" %username)
 
 def projects(request):
   # projects = list(Project.objects.values())
-  return render(request, 'projects.html')
+  projects = Project.objects.all()
+  return render(request, 'projects.html', {
+    'projects': projects,
+  })
 
 def tasks(request):
   # task = Task.objects.get(id=id) # Asi sería sin una buena vista del 404
